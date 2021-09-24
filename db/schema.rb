59d2eefaabd07c4b0479754116210ff28a74b4b5
Christina Delpone <cdelpone@gmail.com>
2021-09-24 08:52:27 -0600

    Complete user story 1

--- db/schema.rb
+++ db/schema.rb
@@ -21,23 +21,6 @@ ActiveRecord::Schema.define(version: 2021_09_24_142833) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "competition_teams", force: :cascade do |t|
-    t.bigint "competition_id"
-    t.bigint "team_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["competition_id"], name: "index_competition_teams_on_competition_id"
-    t.index ["team_id"], name: "index_competition_teams_on_team_id"
-  end
-
-  create_table "competitions", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.string "sport"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "flight_passengers", force: :cascade do |t|
     t.bigint "flight_id"
     t.bigint "passenger_id"
@@ -56,13 +39,6 @@ ActiveRecord::Schema.define(version: 2021_09_24_142833) do
     t.index ["airline_id"], name: "index_flights_on_airline_id"
   end
 
-  create_table "gardens", force: :cascade do |t|
-    t.string "name"
-    t.boolean "organic"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "passengers", force: :cascade do |t|
     t.string "name"
     t.integer "age"
@@ -70,54 +46,7 @@ ActiveRecord::Schema.define(version: 2021_09_24_142833) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "plants", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.integer "days_to_harvest"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "players", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.bigint "team_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["team_id"], name: "index_players_on_team_id"
-  end
-
-  create_table "plot_plants", force: :cascade do |t|
-    t.bigint "plot_id"
-    t.bigint "plant_id"
-    t.index ["plant_id"], name: "index_plot_plants_on_plant_id"
-    t.index ["plot_id"], name: "index_plot_plants_on_plot_id"
-  end
-
-  create_table "plots", force: :cascade do |t|
-    t.integer "number"
-    t.string "size"
-    t.string "direction"
-    t.bigint "garden_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["garden_id"], name: "index_plots_on_garden_id"
-  end
-
-  create_table "teams", force: :cascade do |t|
-    t.string "hometown"
-    t.string "nickname"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_foreign_key "competition_teams", "competitions"
-  add_foreign_key "competition_teams", "teams"
   add_foreign_key "flight_passengers", "flights"
   add_foreign_key "flight_passengers", "passengers"
   add_foreign_key "flights", "airlines"
-  add_foreign_key "players", "teams"
-  add_foreign_key "plot_plants", "plants"
-  add_foreign_key "plot_plants", "plots"
-  add_foreign_key "plots", "gardens"
 end
---
+++ spec/features/flights/index_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+# rspec spec/features/flights/index_spec.rb
+RSpec.describe 'Flight Index Page' do
+  before :each do
+    @airline1 = Airline.create!(name: "Best Airline")
+    @flight1 = @airline1.flights.create!(number: 1, date: "08/03/20", departure_city: "Denver", arrival_city: "Reno")
+    @flight2 = @airline1.flights.create!(number: 2, date: "09/03/20", departure_city: "Atlanta", arrival_city: "Phoenix")
+    @passenger1 = Passenger.create!(name: "Best Passenger", age: 5)
+    @passenger2 = Passenger.create!(name: "Second Passenger", age: 10)
+    @passenger3 = Passenger.create!(name: "Rude Passenger", age: 10)
+    FlightPassenger.create!(flight_id: @flight1.id, passenger_id: @passenger1.id)
+    FlightPassenger.create!(flight_id: @flight1.id, passenger_id: @passenger2.id)
+    FlightPassenger.create!(flight_id: @flight2.id, passenger_id: @passenger3.id)
+
+    visit flights_path
+  end
+
+  describe 'display flights and their attributes' do
+    it 'lists flight numbers, airlines and passengers' do
+      expect(page).to have_content("All Flights")
+      expect(page).to have_content("Flight Number:")
+      expect(page).to have_content("Airline:")
+      expect(page).to have_content("Best Passenger")
+      expect(page).to have_content(@flight1.number)
+      expect(page).to have_content(@flight2.number)
+      expect(@flight1.passengers).to eq([@passenger1, @passenger2])
+      expect(@flight2.passengers).to eq([@passenger3])
+      expect(page).to have_content(@flight2.airline.name)
+    end
+  end
+end
